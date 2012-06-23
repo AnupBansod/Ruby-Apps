f1e8d726647c573a64c9aea6681d003c1e6f3b70
Silent Killer <anup.bansod@gmail.com>
2012-06-24 01:45:16 +0530

    testing for setting spree

---
+++ spree_anup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeAnup::Application.config.secret_token = '9884fdf92c6b90aa21de4553b59afc32a35c23bb2b9252c4e888cf08be2c4926aacde7f827e130d30fe050f3d073bd7bf12b641df6fb00ffc97d0fb51d0e26cf'
